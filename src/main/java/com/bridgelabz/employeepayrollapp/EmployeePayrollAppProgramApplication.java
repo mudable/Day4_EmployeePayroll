fad515f2dc2a160cc0b258b7a52d5a4c4aeee413
mudable <mudableshilpa@gmail.com>
2021-12-23 18:07:56 +0530

    created the employee Payroll project

---
+++ src/main/java/com/bridgelabz/employeepayrollapp/EmployeePayrollAppProgramApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.employeepayrollapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppProgramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeePayrollAppProgramApplication.class, args);
+	}
+
+}
